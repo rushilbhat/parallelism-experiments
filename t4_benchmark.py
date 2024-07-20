296ad975630da3f4fea9c2b358fb4ba0edcaf564
rushilbhat <bhatrushil@gmail.com>
2024-07-20 14:42:38 +0100

    Remove logging

--- t4_benchmark.py
+++ t4_benchmark.py
@@ -327,8 +327,5 @@ for step in range(max_steps):
     tokens_per_sec = tokens_processed / dt
     if master_process:
         print(f"step {step:5d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
-        with open(log_file, "a") as f:
-            f.write(f"{step} train {loss_accum.item():.6f}\n")
-
 if ddp:
     destroy_process_group()
