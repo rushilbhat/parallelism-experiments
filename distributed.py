1e6dd4a70c162304edd70adb8915f765433dd635
rushilbhat <bhatrushil@gmail.com>
2024-08-05 20:28:43 +0100

    Implement deferred initialisation for custom FSDP

--- distributed.py
+++ distributed.py
@@ -1,6 +1,9 @@
 # distributed.py
 import torch
+import torch.nn as nn
 import torch.distributed as dist
+import math
+
 
 class Bucket:
     def __init__(self):
@@ -22,7 +25,7 @@ class Bucket:
         self.gradients.clear()
 
 
-class CustomDDP(torch.nn.Module):
+class CustomDDP(nn.Module):
     def __init__(self, module, process_group, bucket_cap_mb=25): 
         super().__init__()
         self.module = module
@@ -93,3 +96,70 @@ class CustomDDP(torch.nn.Module):
                 param.grad = flat_grads[0][offset:offset+numel].view_as(grad)
                 offset += numel
         bucket.reset()
+
+class CustomFSDP(nn.Module):
+    def __init__(self, module, param_init_fn, world_size, rank):
+        super().__init__()
+        self.module = module
+        self.param_init_fn = param_init_fn
+        self.world_size = world_size
+        self.rank = rank
+        self.is_master = (rank == 0)
+        self.fsdp_units = self._create_fsdp_units_for_gpt(self.module)
+        self.shards = []
+
+        for fsdp_unit in self.fsdp_units:
+            shard = self._create_and_shard_flat_param(fsdp_unit)
+            self.shards.append(shard)
+
+        import sys; sys.exit()
+    def _create_fsdp_units_for_gpt(self, gpt_model):
+        fsdp_units = []
+
+        for block in gpt_model.transformer.h:
+            fsdp_units.append(block)
+            if self.is_master: print([n for n,p in block.named_parameters()])
+
+
+        remaining_params = nn.ModuleDict({
+            "wte": gpt_model.transformer.wte,
+            "wpe": gpt_model.transformer.wpe,
+            "ln_f": gpt_model.transformer.ln_f,
+            "lm_head": gpt_model.lm_head
+        })
+        
+        fsdp_units.append(remaining_params)
+
+        return fsdp_units
+    
+    def _create_and_shard_flat_param(self, fsdp_unit):
+        total_numel = sum(p.numel() for p in fsdp_unit.parameters())
+        padded_size = math.ceil(total_numel / self.world_size) * self.world_size
+        shard_size = padded_size // self.world_size
+
+        if self.is_master:
+            flat_param = nn.Parameter(torch.empty(padded_size, device='cuda'))
+        
+            offset = 0
+            for name, param in fsdp_unit.named_parameters():
+                param_shape = param.shape
+                param_numel = param.numel()
+                param_view = flat_param.data[offset:offset+param.numel()].view(param_shape)
+                
+                name_parts = name.split('.')
+                module = fsdp_unit
+                for part in name_parts[:-1]:
+                    module = getattr(module, part)
+                setattr(module, name_parts[-1], nn.Parameter(param_view))
+                offset += param_numel
+
+            fsdp_unit.apply(self.param_init_fn)
+            if padded_size > total_numel:
+                flat_param.data[total_numel:].zero_()
+            flat_param_shards = list(flat_param.chunk(self.world_size))            
+        
+        shard = torch.empty(shard_size, device='cuda')
+        dist.scatter(shard, flat_param_shards if self.is_master else None, src=0)
+        fsdp_unit.to('meta')
+
+        return shard
