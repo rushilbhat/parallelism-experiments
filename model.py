89441beac53e0d99a9b6a971264214669788b99b
rushilbhat <bhatrushil@gmail.com>
2025-03-18 13:18:35 +0000

    Add head_dim for attention head calculations

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ class CausalSelfAttention(nn.Module):
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
+        self.head_dim = self.n_embd // self.n_head
 
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
@@ -25,10 +26,11 @@ class CausalSelfAttention(nn.Module):
         # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
         # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
         qkv = self.c_attn(x)
-        q, k, v = qkv.split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # q, k, v = qkv.split(self.n_embd, dim=2)
+        q, k, v = torch.chunk(qkv, 3, dim=2)
+        k = k.view(B, T, -1, self.head_dim).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, -1, self.head_dim).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, -1, self.head_dim).transpose(1, 2) # (B, nh, T, hs)
         y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
