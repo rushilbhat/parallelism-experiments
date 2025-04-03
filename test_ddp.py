2944b4c7fe99dbb68ad055bae9843999e2d35b9b
rushilbhat <bhatrushil@gmail.com>
2025-04-03 19:37:26 +0100

    Refactor out common code to ddp and fsdp test suites

--- test_ddp.py
+++ test_ddp.py
@@ -10,53 +10,17 @@ from unittest.mock import patch
 from distributed import CustomDDP, Reducer
 from model import GPT, GPTConfig
 from contextlib import nullcontext
+from test_distributed_base import BaseDistributedTest
 
 torch.use_deterministic_algorithms(True)
 
-class TestCustomDDP(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        dist.init_process_group(backend='nccl')
-        cls.rank = int(os.environ['RANK'])
-        cls.local_rank = int(os.environ['LOCAL_RANK'])
-        cls.world_size = int(os.environ['WORLD_SIZE'])
-
-        assert cls.world_size == 2, "These tests expect only 2 ranks."
-        cls.device = f'cuda:{cls.local_rank}'
-        torch.cuda.set_device(cls.device)
-
-    @classmethod
-    def tearDownClass(cls):
-        dist.destroy_process_group()
-
-    def create_gpt_model(self, device=None, n_layer=1, vocab_size=50304, seed=42):
-        torch.cuda.manual_seed(seed)
-        with torch.device(self.device if device is None else device):
-            model = GPT(GPTConfig(n_layer=n_layer, vocab_size=vocab_size))
-        return model
-
+class TestCustomDDP(BaseDistributedTest):
     def wrap_with_custom_ddp(self, model, bucket_cap_mb=25):
         return CustomDDP(
             module=model,
             process_group=dist.group.WORLD,
             bucket_cap_mb=bucket_cap_mb
         )
-
-    def compare_parameters(self, model1, model2, compare_grads=False):
-        for (name1, param1), (name2, param2) in zip(
-                model1.named_parameters(), model2.named_parameters()
-            ):
-            self.assertEqual(name1, name2)
-            
-            # Compare parameter values
-            if not compare_grads:
-                self.assertTrue(torch.equal(param1, param2))
-            # Compare gradients
-            elif param1.grad is not None and param2.grad is not None:
-                self.assertTrue(torch.equal(param1.grad, param2.grad))
-            else:
-                self.assertTrue(param1.grad is None and param2.grad is None)
-
 
     def test_hooks_registered(self):
         """
--- test_fsdp.py
+++ test_fsdp.py
@@ -11,31 +11,11 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from distributed import CustomFSDP
 from model import GPT, GPTConfig, Block
+from test_distributed_base import BaseDistributedTest
 
 torch.use_deterministic_algorithms(True)
 
-class TestFSDP(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        dist.init_process_group(backend='nccl')
-        cls.rank = int(os.environ['RANK'])
-        cls.local_rank = int(os.environ['LOCAL_RANK'])
-        cls.world_size = int(os.environ['WORLD_SIZE'])
-
-        assert cls.world_size == 2, "These tests expect only 2 ranks."
-        cls.device = f'cuda:{cls.local_rank}'
-        torch.cuda.set_device(cls.device)
-
-    @classmethod
-    def tearDownClass(cls):
-        dist.destroy_process_group()
-
-    def create_gpt_model(self, device=None, n_layer=1, vocab_size=50304, seed=42):
-        torch.cuda.manual_seed(seed)
-        with torch.device(self.device if device is None else device):
-            model = GPT(GPTConfig(n_layer=n_layer, vocab_size=vocab_size))
-        return model
-
+class TestFSDP(BaseDistributedTest):
     def wrap_with_custom_fsdp(self, model):
         return CustomFSDP(
             module=model,
@@ -61,22 +41,6 @@ class TestFSDP(unittest.TestCase):
         actual_change = (post_op_alloc - pre_op_alloc) / (2**20)
         tolerance = abs(expected_change) * tolerance
         self.assertAlmostEqual(actual_change, expected_change, delta=tolerance)
-
-    def compare_parameters(self, model1, model2, compare_grads=False):
-        for (name1, param1), (name2, param2) in zip(
-                model1.named_parameters(), model2.named_parameters()
-            ):
-            self.assertEqual(name1, name2)
-            
-            # Compare parameter values
-            if not compare_grads:
-                self.assertTrue(torch.equal(param1, param2))
-            # Compare gradients
-            elif param1.grad is not None and param2.grad is not None:
-                self.assertTrue(torch.equal(param1.grad, param2.grad))
-            else:
-                self.assertTrue(param1.grad is None and param2.grad is None)
-
 
 
     def test_fsdp_construction(self):
